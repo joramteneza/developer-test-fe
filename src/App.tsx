1e7b0c7448f24c4ceecf6626410b5f87e01ac4e3
Joram Teneza <joram.teneza@gmail.com>
2025-03-20 13:14:03 +0800

    Initial commit: developer test fe

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import { Button } from '@mui/material';
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-blue-100">
+      <Button variant="contained" color="primary">
+        Material UI Button
+      </Button>
+    </div>
+  );
+};
+
+export default App;
